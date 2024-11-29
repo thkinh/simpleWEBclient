a25e361ba048eacf31fd1f5b4ed8e7fafbe64b3a
thkinh <thkinh2008@gmail.com>
2024-11-29 23:04:05 +0700

    update

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -4,14 +4,14 @@ function FetchUser() {
 
     // Fetch user data based on the username
     fetch(`http://192.168.188.15:8080/api/user/getByName?name=${username}`)
-        .then(response => response.json())
+        .then(response => response.json()).catch(error => alert("No users matched that name!!"))
         .then(json => {
-            console.log("json");
+            console.log(json);
             // Update the HTML with the fetched data
             document.getElementById("userUsername").innerText = json.username;
             document.getElementById("userEmail").innerText = json.email;
             document.getElementById("userPassword").innerText = json.password;
 
         })
-        .catch(error => console.log('Error fetching data:', error));
+        .catch(error => alert('Error fetching data:', error));
 }
